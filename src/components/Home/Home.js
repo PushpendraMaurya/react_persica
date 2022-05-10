b2d851d2b01a5221d42def5fa1371a9592960b60
jiit111 <jitumaurya111@gmail.com>
2022-05-10 14:54:05 +0530

    Home screen update

---
+++ src/components/Home/Home.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Home.css";
+import logo from '../../logo.svg';
+import { Navbar, Container ,Carousel} from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top logo"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={logo}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={logo}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={logo}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+};
+
+export default Home;
